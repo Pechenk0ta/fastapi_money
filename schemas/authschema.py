2f6eb3c6ca039eb8f919431392c78e6ca1348676
Pechenk0ta <arseniicharykov@icloud.com>
2023-04-13 00:44:18 +0300

    auth_sistem w/o logout

---
+++ schemas/authschema.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel
+
+
+class userLogin(BaseModel):
+    username: str
+    password: str
---
+++ service/authservice.py
@@ -0,0 +1,16 @@
+from werkzeug.security import generate_password_hash
+from sqlalchemy import select
+from models.users import User_table
+
+def login_in(db, upload, Authorize):
+    if db.execute(select(User_table).where(
+            (User_table.c.username == upload.username) &
+            (User_table.c.password == generate_password_hash(upload.password))
+                    )):
+        access_token = Authorize.create_access_token(subject=upload.username)
+        refresh_token = Authorize.create_refresh_token(subject=upload.username)
+        Authorize.set_access_cookies(access_token)
+        Authorize.set_refresh_cookies(refresh_token)
+        return {"msg": "Successfully login"}
+    else:
+        return {"msg": "Unsuccess"}
